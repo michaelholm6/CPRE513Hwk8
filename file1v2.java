4db59ef252b6842bf5af01a7d4ce586e710c82e2
Michael Holm <mdholm@iastate.edu>
2023-10-19 16:23:58 -0500

    Initial Commit

---
+++ file1v2.java
@@ -0,0 +1,47 @@
+public class file1v2 {
+    
+    public int fun ( int a, int b, String type) {
+
+    if (type.equals("summation")) {
+    return a + b;
+    }
+
+    if (type.equals("multiplication")){
+    return a * b;
+    }
+    
+    int ax = 0;
+    if ( type.equals ("minus") ) {
+    a ++;
+    a --;
+    return a - b ;
+    }
+    if ( type.equals("modulous")) {
+    return a % b ;
+    }
+    int by = 0;
+    if ( type.equals ("addsquare") ) {
+    return ( a * a + b * b ) ;
+    }
+    int bcbc = 1;
+    
+    if ( type.equals ("division") ) {
+    return a / b ;
+    }
+    
+    if ( type.equals ("subsquare") ) {
+    return ( a * a - b * b ) ;
+    }
+    
+    if ( type.equals("addsubsquare") ) {
+    return ((( a + b ) * ( a * b ) ) - (( a - b ) * ( a - b ) ) ) ;
+    }
+    
+    return 1;
+    }
+    
+    public static void main ( String [] args ) {
+    System.out.print(new file1v2().fun(Integer.parseInt(args[0]), Integer.parseInt(args[1]), args[2]));
+    System.out.print("\n");
+    }
+    }
